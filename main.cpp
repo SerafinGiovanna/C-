3d1cfdfe43a0d253a02c4643487d9f6208584c40
SerafinGiovanna <serafingiovanna068@gmail.com>
2026-07-07 17:40:29 -0300

    Exercico

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main() {
+    // ()
+    // + and -
+    // *
+    // /
+    double x = 10;
+    double y = 5;
+    double z;
+    z = (x + 10) / (3 * y);
+    std::cout << z;
+    return 0;
+}
